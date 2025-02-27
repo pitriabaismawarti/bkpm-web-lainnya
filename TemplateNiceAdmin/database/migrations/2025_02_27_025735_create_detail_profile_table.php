a24dcb9960d941a329932cb7c42c7c9999250f5b
pitriabaismawarti <e41231988@student.polije.ac.id>
2025-02-27 10:00:42 +0700

    acara 9

---
+++ TemplateNiceAdmin/database/migrations/2025_02_27_025735_create_detail_profile_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('detail_profile', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('address');
+            $table->string('nomot_tlp');
+            $table->date('ttl');
+            $table->string('foto');
+        $table->timestamps();        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('detail_profile');
+    }
+};
